4a4b4a402b8c000f1f3d2465b1e2967e897e5fb1
songlijiao <ljsong9607@163.com>
2021-02-22 20:23:28 +0800

    part1-module2 大纲 - 对象字面量

--- Part1-Module2/10 - Proxy.js
+++ Part1-Module2/10 - Proxy.js
@@ -1,6 +1,25 @@
 // 捕获属性拦截
 // 2015以前的实现
+let obj1 = {a:1}
+let temp = null;
+Object.defineProperty(obj1,'a',{
+  set(value){
+    console.log("set");
+    temp = value;
+  },
+  get(value){
+    console.log('get');
+    return temp;
+  }
+})
+
+obj1.a = 2;
+console.log(obj1.a)
+
 // 2015的实现：proxy
+let obj2 = {a:1}
+let obj = new Proxy({},)
+
 
 // Proxy的基本用法
 

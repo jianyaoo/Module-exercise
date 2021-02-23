ee7e24f4ae904af61eb11bd329900df16fcb77af
songlijiao <ljsong9607@163.com>
2021-02-23 20:36:11 +0800

    part1-module2 大纲 - Reflect

--- Part1-Module2/10 - Proxy.js
+++ Part1-Module2/10 - Proxy.js
@@ -1,13 +1,14 @@
 // 捕获属性拦截
+// Proxy的基本用法
 // 2015以前的实现
-let obj1 = {a:1}
+let obj1 = {a: 1}
 let temp = null;
-Object.defineProperty(obj1,'a',{
-  set(value){
+Object.defineProperty(obj1, 'a', {
+  set(value) {
     console.log("set");
     temp = value;
   },
-  get(value){
+  get(value) {
     console.log('get');
     return temp;
   }
@@ -17,14 +18,35 @@ obj1.a = 2;
 console.log(obj1.a)
 
 // 2015的实现：proxy
-let obj2 = {a:1}
-let obj = new Proxy({},)
+let obj = new Proxy({}, {
+  get(target, p, receiver) {
+    console.log(`getting ${p}`);
+    return Reflect.get(target, p, receiver)
+  },
+  set(target, p, value, receiver) {
+    console.log(`setting：${p}`);
+    return Reflect.set(target, p, value, receiver)
+  }
+})
+obj.count = 1;
+++obj.count
 
-
-// Proxy的基本用法
+//
 
 // Proxy vs object.defineProperty
 // 1 - 更多的对象操作
+// 01 - get()
+// 02 - set()
+// 03 - has()
+// 04 - ownKeys();
+// 05 - getOwnPropertyDescriptor()
+// 06 - defineProperty()
+// 07 - preventExtensions()
+// 08 - getPrototyOf()
+// 09 - isExtensible
+// 10 - setPrototyof()
+// 11 - apply()
+// 12 - construct()
 
 // 2 - 数组对象的监视
 
--- Part1-Module2/11 - Reflect.js
+++ Part1-Module2/11 - Reflect.js
@@ -1,4 +1,36 @@
 // Reflect
 // 为什么要有一个Reflect？统一提供了一套用于操作对对象的API
+// 01 - 将Object对象的一些明显属于语言内部的方法
+//      比如Object.defineProperty，放到Reflect对象上
+// 02 - 修改某些Object方法的返回结果，让其变得更合理。
+//      比如，Object.defineProperty(obj, name, desc)在无法定义属性时，会抛出一个错误，而Reflect.defineProperty(obj, name, desc)则会返回false。
+// 03 - 让Object操作都变成函数行为。
+//      某些Object操作是命令式，比如name in obj和delete obj[name]，而Reflect.has(obj, name)和Reflect.deleteProperty(obj, name)
+// 04 - Reflect对象的方法与Proxy对象的方法一一对应，只要是Proxy对象的方法，就能在Reflect对象上找到对应的方法。
+//      这就让Proxy对象可以方便地调用对应的Reflect方法，完成默认行为，作为修改行为的基础。也就是说，不管Proxy怎么修改默认行为，你总可以在Reflect上获取默认行为。
 
 // Reflect的十三个方法及对应的方法
+//   Reflect.apply();
+//   Reflect.construct();
+//   Reflect.get(target,name,receive) - 查找并返回target对象的name属性，如果没有该属性则返回undefined
+let obj11 = {
+  a: 1,
+  b: 2,
+  get c() {
+    return this.a + this.b;
+  }
+}
+
+console.log(Reflect.get(obj11, 'a'));
+console.log(Reflect.get(obj11, 'c'));
+console.log(Reflect.get(obj11, 'd'));
+//   Reflect.set()
+//   Reflect.defineProperty();
+//   Reflect.deleteProperty();
+//   Reflect.has();
+//   Reflect.ownKeys();
+//   Reflect.isExtensible();
+//   Reflect.preventExtensions();
+//   Reflect.getOwnPropertyDescriptor();
+//   Reflect.getPrototypeOf();
+//   Reflect.setPrototypeOf();
